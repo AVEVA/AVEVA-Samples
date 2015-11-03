6ecc65bedf2f52e8a6a37df4eb2600d3965dcfcd
rshankar1 <rshankar@osisoft.com>
2015-11-03 10:20:10 -0500

    Formatting changes

--- Basic/Java/src/samples/WaveData.java
+++ Basic/Java/src/samples/WaveData.java
@@ -3,12 +3,12 @@ package samples;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class WaveData {
+public class WaveData
+{
 
 	private int Order;
 	private double Tau;
 	private double Radians;
-
 	private double Sin;
 	private double Cos;
 	private double Tan;
@@ -16,121 +16,102 @@ public class WaveData {
 	private double Cosh;
 	private double Tanh;
 
-	public int getOrder() {
+	public int getOrder() 
+	{
 		return Order;
 	}
 
-
-
-	public void setOrder(int order) {
+	public void setOrder(int order) 
+	{
 		this.Order = order;
 	}
-
-
-
-	public double getTau() {
+	
+	public double getTau()
+	{
 		return Tau;
 	}
 
-
-
-	public void setTau(double tau) {
+	public void setTau(double tau) 
+	{
 		this.Tau = tau;
 	}
 
-
-
-	public double getRadians() {
+	public double getRadians()
+	{
 		return Radians;
 	}
 
-
-
-	public void setRadians(double radians) {
+	public void setRadians(double radians) 
+	{
 		this.Radians = radians;
 	}
-
-
-
-	public double getSin() {
+	
+	public double getSin() 
+	{
 		return Sin;
 	}
 
-
-
-	public void setSin(double sin) {
+	public void setSin(double sin) 
+	{
 		this.Sin = sin;
 	}
 
-
-
-	public double getCos() {
+	public double getCos() 
+	{
 		return Cos;
 	}
 
-
-
-	public void setCos(double cos) {
+	public void setCos(double cos) 
+	{
 		this.Cos = cos;
 	}
 
-
-
-	public double getTan() {
+	public double getTan() 
+	{
 		return Tan;
 	}
 
-
-
-	public void setTan(double tan) {
+	public void setTan(double tan)
+	{
 		this.Tan = tan;
 	}
 
-
-
-	public double getSinh() {
+	public double getSinh() 
+	{
 		return Sinh;
 	}
 
-
-
-	public void setSinh(double sinh) {
+	public void setSinh(double sinh)
+	{
 		this.Sinh = sinh;
 	}
 
-
-
-	public double getCosh() {
+	public double getCosh() 
+	{
 		return Cosh;
 	}
 
-
-
-	public void setCosh(double cosh) {
+	public void setCosh(double cosh)
+	{
 		this.Cosh = cosh;
 	}
 
-
-
-	public double getTanh() {
+	public double getTanh() 
+	{
 		return Tanh;
 	}
 
-
-
-	public void setTanh(double tanh) {
+	public void setTanh(double tanh) 
+	{
 		this.Tanh = tanh;
 	}
-
-
-
 
 	public WaveData()
 	{
 	}
 
-	public WaveData(double multiplier,double  radians,int order ){
-
+	public WaveData(double multiplier,double  radians,int order )
+	{
 		this.Order = order;
 		this.Radians = radians;
 		this.Tau = radians / (2 * Math.PI);
@@ -140,46 +121,32 @@ public class WaveData {
 		this.Sinh = multiplier * Math.sinh(radians);
 		this.Cosh = multiplier * Math.cosh(radians);
 		this.Tanh = multiplier * Math.tanh(radians);
-
 	}
 
-
-
-	public static WaveData next(int interval, double multiplier, int order){
-
+	public static WaveData next(int interval, double multiplier, int order)
+	{
 		Calendar cal =  new GregorianCalendar();
-
-		//double milliSec =  seconds(cal);
 		long milliSec = cal.getTimeInMillis()* 3;
 		int intervalInMilliSec = interval * 60 * 1000;
-
 		double radians = (((double)milliSec % intervalInMilliSec) / intervalInMilliSec ) * 2 * Math.PI;
 
 		return new WaveData(multiplier, radians, order);
-
-
 	}
-
-
-
-	public static double seconds (Calendar cal){
-
+	
+	public static double seconds (Calendar cal)
+	{
 		double sec = 0; 
 		sec += cal.get(Calendar.HOUR_OF_DAY)* 60 * 60* 1000;
-
 		sec += cal.get(Calendar.MINUTE) * 60 * 1000 ;
-
 		sec += cal.get(Calendar.SECOND)* 1000;
-
 		sec += (double)cal.get(Calendar.MILLISECOND);
 
 		return sec;
 	}
 
-
 	@Override
-	public String toString(){
-
+	public String toString()
+	{
 		StringBuilder builder = new StringBuilder();
 		builder.append("Order   = " + Order);
 		builder.append("Radians = " +  Radians);
@@ -191,11 +158,5 @@ public class WaveData {
 		builder.append("Cosh    = "+ Cosh);
 		builder.append("Tanh    = "+ Tanh);
 		return builder.toString();
-
-
-
 	}
-
-
-
 }
