04d5caff168754e09a2eed607174505aaa07779f
rshankar1 <rshankar@osisoft.com>
2015-11-03 09:55:42 -0500

    Formatting changes

--- Basic/Java/src/samples/QiStreamMode.java
+++ Basic/Java/src/samples/QiStreamMode.java
@@ -1,21 +1,21 @@
 package samples;
 
-public enum QiStreamMode {
-	 // Default = Continuous,
-		        Continuous(0),
-		        StepwiseContinuousLeading(1),
-		        StepwiseContinuousTrailing(2),
-		        Discrete(3);
+public enum QiStreamMode
+{
+	 
+        Continuous(0),
+        StepwiseContinuousLeading(1),
+        StepwiseContinuousTrailing(2),
+        Discrete(3);
 	private final int QiStreamMode;
 	
-	private QiStreamMode(int id) {
-	
+	private QiStreamMode(int id)
+	{
 		this.QiStreamMode = id;
 	}
 	
-	public int getValue(){
+	public int getValue()
+	{
 		return QiStreamMode;
-		
 	}
-
 }
