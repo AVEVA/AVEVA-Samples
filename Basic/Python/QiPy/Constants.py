a80cab1c5b3b6f21841545bd7286e57aa2efaf80
gthuruv <gthuruv@users.noreply.github.com>
2015-12-03 15:55:18 -0800

    Comment for QiServerUrl format

--- Basic/Python/QiPy/Constants.py
+++ Basic/Python/QiPy/Constants.py
@@ -4,8 +4,8 @@ class Constants(object):
     
     #VERY IMPORTANT: edit the following values to reflect the authorization items you were given
     authItems = {'resource' : "PLACEHOLDER_REPLACE_WITH_RESOURCE",
-                 'authority' : "PLACEHOLDER_REPLACE_WITH_AUTHORITY",#Ex: "https://login.windows.net/<TENANT-ID>.onmicrosoft.com/oauth2/token,
+                 'authority' : "PLACEHOLDER_REPLACE_WITH_AUTHORITY", #Ex: "https://login.windows.net/<TENANT-ID>.onmicrosoft.com/oauth2/token,
                  'appId' : "PLACEHOLDER_REPLACE_WITH_USER_ID",
                  'appKey' : "PLACEHOLDER_REPLACE_WITH_USER_SECRET"}
     
-    QiServerUrl = "PLACEHOLDER_REPLACE_WITH_QI_SERVER_URL"
+    QiServerUrl = "PLACEHOLDER_REPLACE_WITH_QI_SERVER_URL" #Ex: ApplicationUri:3380 (No http necessary)
