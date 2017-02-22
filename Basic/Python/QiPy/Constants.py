02c7f975b8eb3f1bb9309e6a8ab314bbc9e4765b
Manas Talukdar <mtalukdar@osisoft.com>
2017-02-21 21:38:32 -0800

    Re-added Constants.py

---
+++ Basic/Python/QiPy/Constants.py
@@ -0,0 +1,11 @@
+class Constants(object):
+ -    #VERY IMPORTANT: edit the following values to reflect the authorization items you were given
+ -    authItems = {'resource' : "PLACEHOLDER_REPLACE_WITH_RESOURCE",
+ -                 'authority' : "PLACEHOLDER_REPLACE_WITH_AUTHORITY", #Ex: "https://login.windows.net/<TENANT-NAME>.onmicrosoft.com/oauth2/token,
+ -                 'appId' : "PLACEHOLDER_REPLACE_WITH_USER_ID",
+ -                 'appKey' : "PLACEHOLDER_REPLACE_WITH_USER_SECRET"}
+ -                 
+ -    TenantId = "PLACEHOLDER_REPLACE_WITH_TENANT_ID" #A guid for the Id of the tenant --- NOT the appId
+ -    ###IMPORTANT: For QiServerUrl No "http" is necessary
+ -    ###Ex: "ApplicationUri:443" instead of "https://ApplicationUri:443"
+ -    QiServerUrl = "PLACEHOLDER_REPLACE_WITH_QI_SERVER_URL"
