4d71c9c477d0f6ef4fdb8c4f8c85212456ea4fd2
Jacob Nelson <jnelson@osisoft.com>
2017-06-08 15:53:15 -0700

    Removed keys from java samples

--- Basic/Java/src/samples/Constants.java
+++ Basic/Java/src/samples/Constants.java
@@ -4,21 +4,12 @@ package samples;
 public class Constants {
 
 	//VERY IMPORTANT: edit the following values to reflect the authorization items you were given
-	public final static String _resource = "https://pihomemain.onmicrosoft.com/historian";
-	public final static String _authority = "https://login.windows.net/fluxtesting.onmicrosoft.com"; //Ex: "https://login.windows.net/<TENANT-ID>.onmicrosoft.com";
-	public final static String _appId = "fb252265-0af4-4cda-b26a-567a7e414093";
-	public final static String _appKey = "FmDcXBCqiElvhe1x5DtO/e25avJ2Yk9Zyn0jD90HYm4=";
-	public final static String _qiServerUrl = "http://localhost:8081"; // a Guid (not the app Id)
-	public final static String _tenantId = "b699ae77-0b01-4053-acba-5dfe4b39af88";
-	public final static String _namespaceId = "FluxTesting";
-
-/*	//VERY IMPORTANT: edit the following values to reflect the authorization items you were given
 	public final static String _resource = "PLACEHOLDER_REPLACE_WITH_RESOURCE";
 	public final static String _authority = "PLACEHOLDER_REPLACE_WITH_AUTHORITY"; //Ex: "https://login.windows.net/<TENANT-ID>.onmicrosoft.com";
 	public final static String _appId = "PLACEHOLDER_REPLACE_WITH_USER_ID";
 	public final static String _appKey = "PLACEHOLDER_REPLACE_WITH_USER_SECRET";
 	public final static String _qiServerUrl = "PLACEHOLDER_REPLACE_WITH_QI_SERVER_URL"; // a Guid (not the app Id)
 	public final static String _tenantId = "PLACEHOLDER_REPLACE_WITH_TENANT_ID";
-	public final static String _namespaceId = "PLACEHOLDER_REPLACE_WITH_NAMESPACE_ID";*/
+	public final static String _namespaceId = "PLACEHOLDER_REPLACE_WITH_NAMESPACE_ID";
 
 }
