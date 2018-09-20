4be5b83d3ea5e7fabb4e0fade5a849ecd97cfeb3
Julija Lee <jlee@osisoft.com>
2018-09-20 11:16:54 -0700

    Revert "added copyright"
    This reverts commit b86f587c2417c988129338b7aca1b16181677dae.

--- Basic/DotNet/SdsRestApiCore/Program.cs
+++ Basic/DotNet/SdsRestApiCore/Program.cs
@@ -1,21 +1,4 @@
-// <copyright file="Program.cs" company="OSIsoft, LLC">
-//
-// Copyright (C) 2018 OSIsoft, LLC. All rights reserved.
-//
-// THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF
-// OSIsoft, LLC.  USE, DISCLOSURE, OR REPRODUCTION IS PROHIBITED WITHOUT
-// THE PRIOR EXPRESS WRITTEN PERMISSION OF OSIsoft, LLC.
-//
-// RESTRICTED RIGHTS LEGEND
-// Use, duplication, or disclosure by the Government is subject to restrictions
-// as set forth in subparagraph (c)(1)(ii) of the Rights in Technical Data and
-// Computer Software clause at DFARS 252.227.7013
-//
-// OSIsoft, LLC
-// 1600 Alvarado St, San Leandro, CA 94577
-// </copyright>
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
