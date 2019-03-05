436bc09ff8ca63376226a4d58a205785321979e2
Mitchell Schellenger <mschellenger@osisoft.com>
2019-03-05 14:56:00 -0800

    added example for clientCredentialClient

---
+++ basic_samples/Identity/JavaScript/NodeJs/ClientCredentialClient.js
@@ -0,0 +1,32 @@
+//Copyright 2019 OSIsoft, LLC
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//<http://www.apache.org/licenses/LICENSE-2.0>
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+var restCall = require('request-promise');
+
+var getToken = function () {
+    return restCall({
+        url: 'https://https:dat-b.osisoft.com/identity/connect/token',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            'client_id': 'REPLACE_WITH_CLIENT_ID',
+            'client_secret': 'REPLACE_WITH_CLIENT_SECRET',
+            'grant_type': 'client_credentials',
+        }
+    }).then(function (res) {
+        var obj = JSON.parse(res);
+        return obj.access_token;
+    });
+}
