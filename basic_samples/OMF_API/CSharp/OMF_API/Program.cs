012ae886aea7406dbc6fc8e1420ca68964ce0f82
Derek Endres <dendres@osisoft.com>
2019-03-12 10:52:47 -0400

    Update Program.cs

--- basic_samples/OMF_API/CSharp/OMF_API/Program.cs
+++ basic_samples/OMF_API/CSharp/OMF_API/Program.cs
@@ -64,14 +64,14 @@ namespace OMF_API
             var success = true;
             Exception exc = null;
 
-            Console.WriteLine(" .d88888b.  888b     d888 8888888888        8888888b.  8888888888 .d8888b. 88888888888 ");
-            Console.WriteLine("d88P\" \"Y88b 8888b   d8888 888               888   Y88b 888       d88P  Y88b    888     ");
-            Console.WriteLine("888     888 88888b.d88888 888               888    888 888       Y88b.         888     ");
-            Console.WriteLine("888     888 888Y88888P888 8888888           888   d88P 8888888    \"Y888b.      888     ");
-            Console.WriteLine("888     888 888 Y888P 888 888               8888888P\"  888           \"Y88b.    888     ");
-            Console.WriteLine("888     888 888  Y8P  888 888               888 T88b   888             \"888    888     ");
-            Console.WriteLine("Y88b. .d88P 888   \"   888 888               888  T88b  888       Y88b  d88P    888     ");
-            Console.WriteLine(" \"Y88888P\"  888       888 888      88888888 888   T88b 8888888888 \"Y8888P\"     888     ");
+            Console.WriteLine(" .d88888b.  888b     d888 8888888888               d8888 8888888b. 8888888 ");
+            Console.WriteLine("d88P\" \"Y88b 8888b   d8888 888                     d88888 888   Y88b  888   ");
+            Console.WriteLine("888     888 88888b.d88888 888                    d88P888 888    888  888   ");
+            Console.WriteLine("888     888 888Y88888P888 8888888               d88P 888 888   d88P  888   ");
+            Console.WriteLine("888     888 888 Y888P 888 888                  d88P  888 8888888P\"   888   ");
+            Console.WriteLine("888     888 888  Y8P  888 888                 d88P   888 888         888   ");
+            Console.WriteLine("Y88b. .d88P 888   \"   888 888                d8888888888 888         888   ");
+            Console.WriteLine(" \"Y88888P\"  888       888 888      88888888 d88P     888 888       8888888 ");
 
             try
             {
