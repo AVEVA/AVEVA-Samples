304bfe3825f7f5c315f95b99e295ff27c3faa9d1
Derek Endres <dendres@osisoft.com>
2019-04-24 14:36:26 -0400

    Update protractor.conf.js

--- ocs_samples/basic_samples/SDS/JavaScript/Angular/protractor.conf.js
+++ ocs_samples/basic_samples/SDS/JavaScript/Angular/protractor.conf.js
@@ -20,25 +20,27 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
+    allScriptsTimeout: 11000,
+    specs: [
+        './e2e/**/*.e2e-spec.ts'
     ],
     capabilities: {
-        browserName: 'chrome'
+        'browserName': 'chrome'
     },
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 300000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 300000,
+        print: function () { }
+    },
+    onPrepare() {
+        require('ts-node').register({
+            project: 'e2e/tsconfig.e2e.json'
+        });
+        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    }
 };
 
 
