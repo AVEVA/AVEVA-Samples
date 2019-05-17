b32e3742ab738d888a04997fd84a6b00716b763d
Derek Endres <dendres@osisoft.com>
2019-05-17 09:45:07 -0400

    Update ProgramTest.java

--- omf_samples/basic_samples/OMF_API/Java/omfapijava/src/test/java/com/github/osisoft/omfapi/ProgramTest.java
+++ omf_samples/basic_samples/OMF_API/Java/omfapijava/src/test/java/com/github/osisoft/omfapi/ProgramTest.java
@@ -3,6 +3,7 @@ package com.github.osisoft.omfapi;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import static org.junit.Assert.fail;
 
 /**
  * Unit test for simple App.
@@ -15,6 +16,13 @@ public class ProgramTest
     @Test
     public void shouldAnswerWithTrue()
     {
+        try
+        {
         Program.toRun( true );
+        }
+        catch(Exception e)
+        {
+            fail(e.toString());
+        }
     }
 }
