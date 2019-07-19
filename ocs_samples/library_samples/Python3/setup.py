f2d7b03cc2cea20e7299c7a4e9645efcf21d149b
Christian Foisy <cfoisy@osisoft.com>
2019-07-19 20:21:13 +0000

    Correct lib name

--- ocs_samples/library_samples/Python3/setup.py
+++ ocs_samples/library_samples/Python3/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ocs_sample_library_hub",
+    name="ocs_sample_library_preview",
     version="0.0.30_preview",
     author="OSIsoft",
     license="Apache 2.0",
