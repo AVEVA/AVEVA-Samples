b7f06120fb62d27a9f1c76144c0ef9db96414348
Christian Foisy <cfoisy@osisoft.com>
2019-08-01 15:40:52 +0000

    Bump up version

---
+++ ocs_samples/library_samples/Python3/ocs_sample_library_preview/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ocs_sample_library_preview",
+    version="0.0.32_preview",
+    author="OSIsoft",
+    license="Apache 2.0",
+    author_email="dendres@osisoft.com",
+    description="A preview of an OCS (OSIsoft Cloud Services) client library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/osisoft/OSI-Samples",
+    packages=setuptools.find_packages(),    
+    install_requires=[
+        'requests==2.22.0',
+        'python-dateutil==2.8.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
